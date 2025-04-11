aa1fbf689f6db8dc089240f2a5ad8696a0b1c21c
divya-bhayani <divyabhayani20@gmail.com>
2025-04-11 14:27:17 +0530

    sss

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Ceritified from "./Components/Admin/Cerified_diamond/Ceritified";
 import { store } from "./Store/Store";
 import { Provider, useSelector } from "react-redux";
 import Search_Result from "./Components/Admin/Search_result/Search_Result";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   // useEffect(async () => {
@@ -13,8 +14,12 @@ function App() {
 
   return (
     <Provider store={store}>
-      {/* <Ceritified /> */}
-      <Search_Result />
+      <Routes>
+        <Route path="/" element={<Ceritified />} />
+        <Route path="/Search_result" element={<Search_Result />} />
+      </Routes>
+
+      {/* <Search_Result /> */}
     </Provider>
   );
 }
